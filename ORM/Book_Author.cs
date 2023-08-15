12eb4bf91dbfd3ae3e5c8af52e0b4b498f4cd1f4
Jiyeon Heo <jiyeon.heo0906@gmail.com>
2023-08-14 18:35:41 -0600

    Change skeletop files

---
+++ ORM/Book_Author.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM
+{
+	public class Book_Author
+	{
+		public int Id_author { get; set; }
+		public int Id_book { get; set; }
+
+		public Book_Author() { }
+		public Book_Author(int id_author, int id_book)
+		{
+			Id_author = id_author;
+			Id_book = id_book;
+		}	
+	}
+}
---
+++ ORM/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM
+{
+	public class Category
+	{
+		public int Id_category { get; set; }
+		public string Name_category { get; set; }
+
+		public Category() { }
+		public Category(int id_category, string name_category)
+		{
+			Id_category = id_category;
+			Name_category = name_category;
+		}	
+	}
+}
